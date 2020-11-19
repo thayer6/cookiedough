a258fa43807e3104cb5dd353e974053bb0c24266
thayer6 <casey.thayer6@gmail.com>
2020-11-19 08:43:21 -0800

    fix logged in user view and update user info

--- cookiedough/users/models.py
+++ cookiedough/users/models.py
@@ -9,6 +9,7 @@ class User(AbstractUser):
 
     #: First and last name do not cover name patterns around the globe
     name = CharField(_("Name of User"), blank=True, max_length=255)
+    bio = CharField(("Short Bio"), blank=True, max_length=500)
 
     def get_absolute_url(self):
         """Get url for user's detail view.
