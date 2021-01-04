c419b476665d5a59b5ad6941ff194e4f546d44c1
thayer6 <casey.thayer6@gmail.com>
2021-01-04 17:50:06 +0000

    add shell script to startup app

---
+++ migrate-run.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+export CELERY_BROKER_URL=redis://localhost:6379/0
+
+# migrate db
+python manage.py migrate
+
+# make db migrations
+python manage.py makemigrations
+
+# run server
+python manage.py runserver
